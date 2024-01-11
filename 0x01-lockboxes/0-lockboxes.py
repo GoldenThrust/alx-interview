3578401015574f9d8847d53e82c627dbd7655351
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-01-11 09:58:42 +0100

    find the keys and unlocked the boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
+""" Can unlocked All """
+
 
 def canUnlockAll(boxes):
-    unlocked_boxes = list([0])
+    """
+    Determines whether it is possible to unlock all boxes in a given list.
+
+    Args:
+    - boxes (list of lists): A list where each index represents a box, and the value at that index is a list of keys.
+
+    Returns:
+    - bool: True if all boxes can be unlocked, False otherwise.
+
+    Examples:
+    >>> canUnlockAll([[1], [2], [3], [4], []])
+    True
+    >>> canUnlockAll([[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]])
+    False
+    """
+
+    unlocked_boxes = [0]
 
     for box in unlocked_boxes:
         for key in boxes[box]:
