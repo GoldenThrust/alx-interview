b106c91cf469cd81c1657fe078d6b1b254072c6c
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-01-11 09:42:59 +0100

    find the keys and unlocked the boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,9 @@
+def canUnlockAll(boxes):
+    unlocked_boxes = list([0])
+
+    for box in unlocked_boxes:
+        for key in boxes[box]:
+            if key not in unlocked_boxes and key < len(boxes):
+                unlocked_boxes.append(key)
+
+    return len(unlocked_boxes) == len(boxes)
