e6a21721537f4bf3034dfdf8cf45d01f34633848
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-01-18 13:34:23 +0100

    Make code shorter

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -16,56 +16,17 @@ def minOperations(n):
     if n <= 1:
         return 0
 
-    num_char = 2
-    num_oper = 2
-    copied_len = 1
+    num_oper = 0
+    copied_len = 0
+    num_char = 1
 
-    for i in range(2, n):
-        if n % i == 0 and num_char <= n // 2:
-            copied_len, num_oper, num_char = copy_paste(
-                copied_len, num_oper, num_char)
-        else:
-            copied_len, num_oper, num_char = paste(
-                copied_len, num_oper, num_char)
-
-        if num_char >= n:
-            break
+    while num_char < n:
+        if n % num_char == 0:  # copy and paste
+            copied_len = num_char
+            num_char += copied_len
+            num_oper += 2
+        else:  # paste only
+            num_char += copied_len
+            num_oper += 1
 
     return num_oper
-
-
-def copy_paste(copied_len, num_oper, num_char):
-    """
-    Copy and paste operation.
-
-    Parameters:
-    - copied_len (int): Length of the copied text.
-    - num_oper (int): Current number of operations.
-    - num_char (int): Current number of characters.
-
-    Returns:
-    - List[int]: Updated values for copied_len, num_oper, and num_char.
-    """
-    num_oper += 2
-    copied_len = num_char
-    num_char += copied_len
-
-    return [copied_len, num_oper, num_char]
-
-
-def paste(copied_len, num_oper, num_char):
-    """
-    Paste operation.
-
-    Parameters:
-    - copied_len (int): Length of the copied text.
-    - num_oper (int): Current number of operations.
-    - num_char (int): Current number of characters.
-
-    Returns:
-    - List[int]: Updated values for copied_len, num_oper, and num_char.
-    """
-    num_oper += 1
-    num_char += copied_len
-
-    return [copied_len, num_oper, num_char]
