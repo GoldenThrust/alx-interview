edd4e4d7ec5c69c22b69aad491cc0f3dc7d8afcb
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-01-25 16:52:54 -0800

    Revert "No my Code"
    This reverts commit b2866735506c9bd8d5e87ef69dea9dac7c8901e5.

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -24,27 +24,30 @@ import sys
 
 
 try:
-    my_dict = {}
     total_size = 0
+    status_codes_count = {}
     for i, line in enumerate(sys.stdin, start=1):
-        parts = line.split(" ")
         try:
-            total_size += int(parts[-1])
-            status = int(parts[-2])
-            if status not in my_dict:
-                my_dict[status] = 1
+            status_code, file_size = line.split()[-2:]
+            status_code = int(status_code)
+
+            total_size += int(file_size)
+
+            if status_code in status_codes_count:
+                status_codes_count[status_code] += 1
             else:
-                my_dict[status] += 1
+                status_codes_count[status_code] = 1
+
         except (ValueError, IndexError):
             continue
-        my_dict = dict(sorted(my_dict.items()))
+
         if i % 10 == 0:
             print("File size: {}".format(total_size))
-            for key, val in my_dict.items():
-                print("{}: {}".format(key, val))
+            for code in sorted(status_codes_count.keys()):
+                print("{}: {}".format(code, status_codes_count[code]))
 except KeyboardInterrupt:
     pass
 finally:
     print("File size: {}".format(total_size))
-    for key, val in my_dict.items():
-        print("{}: {}".format(key, val))
+    for code in sorted(status_codes_count.keys()):
+        print("{}: {}".format(code, status_codes_count[code]))
