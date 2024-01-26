f3ba4e9864abe3f3c613d63db2324820675ff2d4
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-01-25 16:36:37 -0800

    No Commit Message

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,7 +20,6 @@ Example Log Entry:
 
 """
 
-import re
 import sys
 
 total_size = 0
@@ -28,60 +27,32 @@ status_codes_count = {}
 lines = 0
 
 
-def print_log():
-    """
-    Prints the total file size and count of each HTTP status code.
-    """
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes_count.keys()):
-        print("{}: {}".format(code, status_codes_count[code]))
-
-
-def parse_log(log):
-    """
-    Parse a log entry and update global variables.
-
-    Args:
-        log (str): Log entry string.
-
-    Returns:
-        bool: True if parsing is successful, False otherwise.
-    """
-    global total_size, lines
-    try:
-        pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' \
-            r'\[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
-
-        match = re.match(pattern, log)
-        if not match:
-            raise Exception
-
-        status_code, file_size = log.split()[-2:]
-        status_code = int(status_code)
-
-        total_size += int(file_size)
-
-        if status_code in status_codes_count:
-            status_codes_count[status_code] += 1
-        else:
-            status_codes_count[status_code] = 1
-
-        lines += 1
-
-        return True
-    except (ValueError, IndexError):
-        return False
-
-
 if __name__ == '__main__':
     try:
         for line in sys.stdin:
-            if not parse_log(line):
+            try:
+                status_code, file_size = line.split()[-2:]
+                status_code = int(status_code)
+
+                total_size += int(file_size)
+
+                if status_code in status_codes_count:
+                    status_codes_count[status_code] += 1
+                else:
+                    status_codes_count[status_code] = 1
+
+                lines += 1
+
+            except (ValueError, IndexError):
                 continue
 
             if lines % 10 == 0:
-                print_log()
+                print("File size: {}".format(total_size))
+                for code in sorted(status_codes_count.keys()):
+                    print("{}: {}".format(code, status_codes_count[code]))
     except KeyboardInterrupt:
         pass
     finally:
-        print_log()
+        print("File size: {}".format(total_size))
+        for code in sorted(status_codes_count.keys()):
+            print("{}: {}".format(code, status_codes_count[code]))
