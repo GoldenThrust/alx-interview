5314db6f7e3e3896b6b8c234e3344384bf0fbeae
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-08 18:10:55 +0100

    N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 """
 N-Queens Algorithmn
+This script solves the N-Queens problem for a given board size N.
+The N-Queens problem involves placing N
+queens on an N*N chessboard in such a way that
+no two queens threaten each other.
+
+Usage:
+    python nqueens.py N
+
+Arguments:
+    N (int): The size of the chessboard and the number of queens to be placed.
+
+Example:
+>>> ./nqueens.py 4
+[[0, 1], [1, 3], [2, 0], [3, 2]]
+[[0, 2], [1, 0], [2, 3], [3, 1]]
+
 """
 
 import sys
@@ -8,7 +24,10 @@ import sys
 
 def main():
     """
-    Main function to solve the N-Queens
+    Main function to solve the N-Queens problem.
+
+    This function reads the board size N from the command-line argument, validates it,
+    and initiates the backtracking algorithm to find and print solutions to the N-Queens problem.
     """
 
     if len(sys.argv) != 2:
@@ -32,7 +51,14 @@ def main():
 
 def is_safe(pos, queen_pos):
     """
-    Check safe placing of queen
+    Check if placing a queen at a given position is safe.
+
+    Args:
+        pos (list): The position [row, col] to check.
+        queen_pos (list): List of queen positions.
+
+    Returns:
+        bool: True if safe, False otherwise.
     """
 
     for j in reversed(range(pos[0] + 1)):
@@ -47,7 +73,12 @@ def is_safe(pos, queen_pos):
 
 def backtrack(row, queen_pos, num):
     """
-    Backtrack to find all solutions
+    Backtrack to find all solutions for placing queens on the board.
+
+    Args:
+        row (int): The current row.
+        queen_pos (list): List of queen positions.
+        num (int): The size of the chessboard.
     """
 
     if row == num:
