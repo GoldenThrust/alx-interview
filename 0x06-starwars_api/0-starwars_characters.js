8b263c8220d12ec1b282cf8164ba3952fab0c109
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-15 03:17:28 +0100

    StarWar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,18 +4,18 @@ const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api/films';
 
 if (process.argv.length > 2) {
-    request(`${API_URL}/${process.argv[2]}`, (err, res, body) => {
+    request(`${API_URL}/${process.argv[2]}/`, (err, res, body) => {
         if (err) {
             console.log(err);
         } else {
             const data = JSON.parse(body).characters;
             if (data) {
                 for (const url of data) {
-                    request(url, (err, res, body) => {
-                        if (err) {
+                    request(url, (nErr, nRes, nBody) => {
+                        if (nErr) {
                             console.log(err);
                         } else {
-                            console.log(JSON.parse(body).name);
+                            console.log(JSON.parse(nBody).name);
                         }
                     });
                 }
